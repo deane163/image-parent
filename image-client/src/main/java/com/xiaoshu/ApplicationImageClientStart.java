6bf7b114b5b839a2954785c748a3a8f7c75678d2
deane163 <deane163@126.com>
2017-11-30 17:26:47 +0800

    【功能】添加字符拦截功能UTF-8

--- image-client/src/main/java/com/xiaoshu/ApplicationImageClientStart.java
+++ image-client/src/main/java/com/xiaoshu/ApplicationImageClientStart.java
@@ -5,10 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
  * The Image client for single image upload and batch images upload
+ * 
  * @author Administrator
  *
  */
@@ -26,5 +30,20 @@ public class ApplicationImageClientStart extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		logger.info("start The Image client ...");
 		SpringApplication.run(ApplicationImageClientStart.class, args);
+	}
+
+	/**
+	 * 字符拦截，使用UTF-8编码
+	 * 
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		registrationBean.setFilter(characterEncodingFilter);
+		registrationBean.addUrlPatterns("/*");
+		return registrationBean;
 	}
 }
--- image-server/src/main/java/com/xiaoshu/ApplicationImageServerStart.java
+++ image-server/src/main/java/com/xiaoshu/ApplicationImageServerStart.java
@@ -5,10 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
- * 图片服务器启动类
+ * 图片服务器启动类，负责上传的操作
  * @author Administrator
  *
  */
@@ -25,5 +28,20 @@ public class ApplicationImageServerStart extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		logger.info("start The Image Server ...");
 		SpringApplication.run(ApplicationImageServerStart.class, args);
+	}
+	
+	/**
+	 * 字符拦截，使用UTF-8编码
+	 * 
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		registrationBean.setFilter(characterEncodingFilter);
+		registrationBean.addUrlPatterns("/*");
+		return registrationBean;
 	}
 }
