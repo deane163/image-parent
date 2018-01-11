460ff15b2cd79b2bc17f289cc04d69475d3a170c
deane163 <deane163@126.com>
2018-01-11 11:33:34 +0800

    【功能】添加注解信息

--- image-server/src/main/java/com/xiaoshu/annotation/Authorization.java
+++ image-server/src/main/java/com/xiaoshu/annotation/Authorization.java
@@ -7,9 +7,38 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2018年1月11日上午11:32:38
+ * 
+ * Copyright (C)2013-2018 小树盛凯科技 All rights reserved.
+ */
 @Target({TYPE, METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Authorization {
-
+	
+	String value();
 }
